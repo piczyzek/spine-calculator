ac6184e96ff9d81ce994b7d5cc18eb2055485023
Maciek <piczyzek@gmail.com>
2017-04-18 15:17:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
-$(document).ready(function() {
-    
+$( document ).ready(function() {
+    var thickness = value/100;
+    var volume = input/2;
+    var paperboard = input2;
+  if (hardcover = selected){ 
+    document.getElementById("hrdcvr").removeAttribute("twarda");
+      if (round = selected){
+          console.log((thickness * volume + paperboard)* 1.1);}
+      if else (straight = selecter){
+          console.log(thickness * volume + paperboard);}
+      else {
+          alert("Choose spine style!");}
+      }
+  else {
+    console.log(thickness * volume);
+  }
 });

2eca722d676cbafea3e7f9d92036a49f7bef2b87
Maciek <piczyzek@gmail.com>
2017-04-19 13:15:58 +0200

    gowno

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 $( document ).ready(function() {
-    var thickness = value/100;
-    var volume = input/2;
+      var thickness = $( "#paper_type" ).val();
+
+
+    /**var volume = input/2;
     var paperboard = input2;
   if (hardcover = selected){ 
     document.getElementById("hrdcvr").removeAttribute("twarda");
       if (round = selected){
           console.log((thickness * volume + paperboard)* 1.1);}
-      if else (straight = selecter){
+      if else (straight = selected){
           console.log(thickness * volume + paperboard);}
       else {
           alert("Choose spine style!");}
       }
   else {
     console.log(thickness * volume);
-  }
+  }**/
+  
 });

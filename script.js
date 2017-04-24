c0bb83263f7a0b2dfc63ccb202884efbf71a7c38
Maciek <piczyzek@gmail.com>
2017-04-24 09:31:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,31 @@
-$( document ).ready(function() {
-      var thickness = $( "#paper_type" ).val();
+  document.getElementById('inlineRadio1').disabled = true;
+  document.getElementById('inlineRadio2').disabled = true;
+  document.getElementById('paperboard').disabled = true;
 
+  var countOnSubmit = function(ev) {
+    var material = document.getElementById('material');
+    var pageCount = document.getElementById('pageCount');
+    var paperboard = document.getElementById('paperboard');
+    var spine = ((pageCount.value / 2) * (material.value / 1000));
 
-    /**var volume = input/2;
-    var paperboard = input2;
-  if (hardcover = selected){ 
-    document.getElementById("hrdcvr").removeAttribute("twarda");
-      if (round = selected){
-          console.log((thickness * volume + paperboard)* 1.1);}
-      if else (straight = selected){
-          console.log(thickness * volume + paperboard);}
-      else {
-          alert("Choose spine style!");}
-      }
-  else {
-    console.log(thickness * volume);
-  }**/
-  
-});
+    if ($("#inlineRadio1").is(":checked")) {
+      document.getElementById('tag').innerHTML = +((spine + (paperboard.value * 2)) * 1.1).toFixed(2);
+    } else if ($("#inlineRadio2").is(":checked")) {
+      document.getElementById('tag').innerHTML = +(spine + (paperboard.value * 2)).toFixed(2);
+    } else {
+      document.getElementById('tag').innerHTML = +spine.toFixed(2);
+    };
+    return false;
+  };
+
+  var checkBox = document.geElementById('checkBox');
+
+  function checkBoxOnChange() {
+    $(document.getElementById('hardcoverPart')).slideToggle('slow');
+    $(document.getElementById('inlineRadio1')).prop("checked", false);
+    $(document.getElementById('inlineRadio2')).prop("checked", false);
+    document.getElementById('inlineRadio1').disabled = false;
+    document.getElementById('inlineRadio2').disabled = false;
+    document.getElementById('paperboard').disabled = false;
+  };
+
